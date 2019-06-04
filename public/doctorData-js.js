fc390bf6eb06aa58f0cb18aad5c61596538307af
amit <amit.2016@iitg.ac.in>
2019-06-05 03:53:08 +0530

    Merged diagnosis

--- public/doctorData-js.js
+++ public/doctorData-js.js
@@ -1,18 +1,18 @@
 function addDoc(){
-    const name = document.getElementById("name") ;
-    const city = document.getElementById("city") ;
-    const college = document.getElementById("college") ;
-    const age = document.getElementById("age") ;
-    const gender = document.getElementById("gender") ;
-    const degree = document.getElementById("degree") ;
-    const experience = document.getElementById("experience") ;
-    const speciality = document.getElementById("speciality") ;
-    const clinic = document.getElementById("clinic") ;
-    const licenceno = document.getElementById("licenceno") ;
-    const description = document.getElementById("description") ;
-    const contact = document.getElementById("contact") ;
-    const email = document.getElementById("email") ;
-    const password = document.getElementById("password") ;
+    const name = document.getElementById("name").value) ;
+    const city = document.getElementById("city").value ;
+    const college = document.getElementById("college").value ;
+    const age = document.getElementById("age").value ;
+    const gender = document.getElementById("gender").value ;
+    const degree = document.getElementById("degree").value ;
+    const experience = document.getElementById("experience").value ;
+    const speciality = document.getElementById("speciality").value ;
+    const clinic = document.getElementById("clinic").value ;
+    const licenceno = document.getElementById("licenceno").value ;
+    const description = document.getElementById("description").value ;
+    const contact = document.getElementById("contact").value ;
+    const email = document.getElementById("email").value ;
+    const password = document.getElementById("password").value ;
     axios.post('http://localhost:3000/doctors', {
           name : name ,
           city : city ,
